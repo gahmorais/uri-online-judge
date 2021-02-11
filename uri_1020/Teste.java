d0077bc77eba158c7938f26110809a754f7fd7c6
gahmorais <grm270692@hotmail.com>
2021-02-11 11:00:43 -0300

    Exercicios 1019_2 e 1020 incompletos

---
+++ uri_1020/Teste.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Teste {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in)
+        int dias = sc.nextInt();
+
+        for(int i = 1; i< dias;i++){
+            Main.calcularAnosEmDias( i );
+        }
+    }
+}
